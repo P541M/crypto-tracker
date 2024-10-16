6ff7e530da2f4d1acd5259731264b0e427cc0d61
P541M <videna.psalmeleazar@gmail.com>
2024-10-16 18:54:13 -0400

    feat: git rid of mobile modal

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import LandingPage from "./components/LandingPage";
 import CryptoListPage from "./components/CryptoListPage";
-import MobileViewMessage from "./components/MobileViewMessage";
 
 function App() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    const isHandheldDevice =
-      /android|ipad|iphone|ipod|windows phone|opera mini|iemobile/i.test(
-        userAgent.toLowerCase()
-      );
-    setIsMobile(isHandheldDevice || window.innerWidth < 768);
-
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  if (isMobile) {
-    return <MobileViewMessage />;
-  }
-
   return (
     <div className="App relative min-h-screen bg-bg text-text overflow-hidden">
       <div className="absolute inset-0 z-0">
